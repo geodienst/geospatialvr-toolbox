616ed98e4bfb9767814fdba9090b19e64640e5a2
niekvanruler <n.j.van.ruler@rug.nl>
2023-06-28 16:06:16 +0200

    areas of interest feature class wordt toegevoegd aan de gdb buildings en areasofinterest worden op de kaart geplot

--- import_and_footprint.py
+++ import_and_footprint.py
@@ -56,6 +56,15 @@ def main(obj_input_dir, output_gdb_path, output_gdb_name, floor_height):
 
     arcpy.AddMessage("Buildings feature class {0} created".format(outputfc2d))
 
+    # add feature class for area of interest
+    arcpy.management.CreateFeatureclass(fullpathgdb, "areaofinterest", "POLYGON", None, "DISABLED", "DISABLED",
+                                        'PROJCS["RD_New",GEOGCS["GCS_Amersfoort",DATUM["D_Amersfoort",SPHEROID["Bessel_1841",6377397.155,299.1528128]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Double_Stereographic"],PARAMETER["False_Easting",155000.0],PARAMETER["False_Northing",463000.0],PARAMETER["Central_Meridian",5.38763888888889],PARAMETER["Scale_Factor",0.9999079],PARAMETER["Latitude_Of_Origin",52.15616055555555],UNIT["Meter",1.0]];-30515500 -30279500 10000;-100000 10000;-100000 10000;0.001;0.001;0.001;IsHighPrecision', '', 0, 0, 0, '')
+
+    # add buildings fc and areasofinterest fc to map
+    aprx = arcpy.mp.ArcGISProject("CURRENT")
+    aprxMap = aprx.listMaps("Map")[0]
+    aprxMap.addDataFromPath(outputfc2d)
+    aprxMap.addDataFromPath(fullpathgdb + "/" + "areaofinterest")
 
 def showProgress(text, step):
     arcpy.SetProgressorLabel(text)
