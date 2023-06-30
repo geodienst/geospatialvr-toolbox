7d2a4af3254767cfcf573042c89a21f74455b7e4
niekvanruler <n.j.van.ruler@rug.nl>
2023-06-30 13:38:05 +0200

    melding van de actie op het scherm

--- createzip.ps1
+++ createzip.ps1
@@ -1,2 +1,4 @@
 #
 Compress-Archive -Path index.tbx,objimport.py,objsplit.py,calculate_fsi.py,calculate_gsi.py,import_and_footprint.py -DestinationPath download/geospatialvr-toolbox.zip -Force
+
+Write-Output 'Zip file created for distribution'
